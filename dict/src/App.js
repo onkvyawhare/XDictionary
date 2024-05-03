174cfa741abc22f07288c8952a0d6140c8eb1939
onkvyawhare <onkvyawhare@gmail.com>
2024-05-03 17:03:22 +0530

    final commit

--- dict/src/App.js
+++ dict/src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css"; // Import CSS file for styling
 
-function App() {
+const XDictionar = () => {
+  const [dictionary] = useState([
+    {
+      word: "React",
+      meaning: "A JavaScript library for building user interfaces.",
+    },
+    { word: "Component", meaning: "A reusable building block in React." },
+    { word: "State", meaning: "An object that stores data for a component." },
+  ]);
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResult, setSearchResult] = useState("");
+
+  const handleSearch = () => {
+    const searchTermLower = searchTerm.toLowerCase();
+    const foundWord = dictionary.find(
+      (entry) => entry.word.toLowerCase() === searchTermLower
+    );
+
+    setSearchResult(
+      foundWord ? foundWord.meaning : "Word not found in the dictionary."
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Dictionary App</h1>
+      <div className="search-container">
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Enter search term"
+        />
+        <button onClick={handleSearch}>Search</button>
+      </div>
+      <div className="result-container">
+        <p class="para">Definition:</p>
+        <p>{searchResult}</p>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default XDictionar;
